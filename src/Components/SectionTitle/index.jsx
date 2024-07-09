ad48ee01201d3dfbf7957efecb085aacdc6ebce4
miquelven <miquelven.silva@gmail.com>
2024-07-09 14:14:38 -0300

    feat: create sectiontitle component

---
+++ src/Components/SectionTitle/index.jsx
@@ -0,0 +1,7 @@
+export default function SectionTitle({ children }) {
+  return (
+    <h2 className="relative inline  text-4xl w-fit font-bold after:content-[''] after:w-20 after:h-1 after:absolute after:-bottom-1 after:right-1/2 after:translate-x-1/2 after:bg-black">
+      {children}
+    </h2>
+  );
+}
