b785640310ac2e2f0c8c9b4ece501fb40de053ff
miquelven <miquelven.silva@gmail.com>
2024-07-09 20:33:05 -0300

    style: add id and change z index

--- src/Components/Collections/index.jsx
+++ src/Components/Collections/index.jsx
@@ -6,13 +6,13 @@ import WhatsappButton from "../WhatsappButton";
 
 export default function Collections() {
   return (
-    <section className="mt-10 mb-40">
+    <section id="colecoes" className="mt-10 mb-40">
       <Container>
         {/* line */}
         <div className="bg-gradient-to-b from-zinc-400/10 to-[#e9ecef] w-full h-2 mb-20"></div>
 
-        <div className="relative grid grid-cols-3 justify-center items-center max-lg:grid-cols-2 max-sm:grid-cols-1">
-          <div className=" flex flex-col gap-20 max-sm:relative max-sm:z-20 max-sm:-mt-14">
+        <div className="relative z-[-1] grid grid-cols-3 justify-center items-center max-lg:grid-cols-2 max-sm:grid-cols-1">
+          <div className=" flex flex-col gap-20 max-sm:relative max-sm:z-10  max-sm:-mt-14">
             <SectionTitle>Nova Coleção</SectionTitle>
 
             <div className="flex flex-col gap-8 ">
@@ -30,7 +30,7 @@ export default function Collections() {
           <img
             src={collectionImg}
             alt="Imagem de um homem de terno"
-            className="shadow-md shadow-black/20 rounded-md max-sm:absolute max-sm:top-0 max-sm:right-0  max-sm:h-80 max-sm:brightness-75"
+            className="shadow-md shadow-black/20 rounded-md max-sm:absolute max-sm:top-10 max-sm:right-0  max-sm:h-80 max-sm:brightness-75"
           />
         </div>
       </Container>
