f0ea041c322452ef7c88b04ce1c040cf7f7e377e
miquelven <miquelven.silva@gmail.com>
2024-07-09 20:36:37 -0300

    feat: create reviewdata file

---
+++ src/data/reviewData.js
@@ -0,0 +1,41 @@
+import reviewMan1 from "../assets/imgs/reviewMan1.png";
+import reviewMan2 from "../assets/imgs/reviewMan2.png";
+
+import reviewWomen1 from "../assets/imgs/reviewWomen1.png";
+import reviewWomen2 from "../assets/imgs/reviewWome2.png";
+import reviewWomen3 from "../assets/imgs/reviewWomen3.jpg";
+
+const reviewData = [
+  {
+    id: 1,
+    name: "Maria Fernandes",
+    text: "Adorei a qualidade das roupas! Chegaram super rápido e com um ótimo acabamento. Recomendo!",
+    src: reviewWomen1,
+  },
+  {
+    id: 2,
+    name: "João Oliveira",
+    text: "O site é fácil de navegar e as peças são lindas. Comprei uma jaqueta e fiquei muito satisfeito com a compra.",
+    src: reviewMan1,
+  },
+  {
+    id: 3,
+    name: "Ana Paula Costa",
+    text: "Excelente atendimento ao cliente. Tive um problema com o tamanho e resolveram rapidamente. Voltarei a comprar!",
+    src: reviewWomen2,
+  },
+  {
+    id: 4,
+    name: "Lucas Silva",
+    text: "Gostei muito das tendências que encontrei aqui. As roupas são modernas e bem estilosas. Vale a pena!",
+    src: reviewMan2,
+  },
+  {
+    id: 5,
+    name: "Camila Santos",
+    text: "Amei as promoções e a variedade de produtos. As peças são confortáveis e vestem super bem. Recomendadíssimo!",
+    src: reviewWomen3,
+  },
+];
+
+export default reviewData;
