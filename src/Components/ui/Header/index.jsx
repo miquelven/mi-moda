aa5dda1d31bea3f8fb0f26e17a8e6c6dbe6c6d56
miquelven <miquelven.silva@gmail.com>
2024-07-09 20:36:15 -0300

    feat: close menu when clicked in link

--- src/Components/ui/Header/index.jsx
+++ src/Components/ui/Header/index.jsx
@@ -3,10 +3,18 @@ import linksData from "../../../data/linksData";
 
 import { FaBarsStaggered } from "react-icons/fa6";
 import { MdOutlineClose } from "react-icons/md";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Header() {
   const [openMenu, setOpenMenu] = useState(false);
+
+  useEffect(() => {
+    if (openMenu) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  });
 
   return (
     <header className="py-5 shadow-lg shadow-zinc-300/50 relative z-10">
@@ -16,15 +24,18 @@ function Header() {
 
           <nav>
             <ul
-              className={`transition-all duration-300 flex items-center max-md:absolute max-md:h-screen max-md:inset-0 max-md:flex-col max-md:py-20 max-md:items-start max-md:gap-14 max-md:bg-[#e9ecef] 
+              className={`transition-all duration-300 flex items-center max-md:fixed max-md:h-screen max-md:inset-0 max-md:flex-col max-md:py-20 max-md:items-start max-md:gap-14 max-md:bg-[#e9ecef] 
                      ${
-                       !openMenu ? " max-md:opacity-0 z-0" : " opacity-100 z-20"
+                       !openMenu
+                         ? "  max-md:opacity-0 z-10"
+                         : "  opacity-100 z-30"
                      }
                     `}
             >
               {linksData.map((link) => (
                 <li
                   key={link.id}
+                  onClick={() => setOpenMenu(!openMenu)}
                   className="transition-all duration-300 px-7 hover:text-[#ef8354]  "
                 >
                   <span className="text-lg font-semibold max-md:text-3xl max-md:font-normal">
@@ -35,7 +46,7 @@ function Header() {
             </ul>
             <button
               onClick={() => setOpenMenu(!openMenu)}
-              className="relative z-20 p-2 hidden max-md:block"
+              className="relative z-40 p-2 hidden max-md:block"
             >
               {openMenu ? (
                 <MdOutlineClose className="w-6 h-6" />
