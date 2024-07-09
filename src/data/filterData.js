6d31d1d0ea43b94cb655a5c517c49548162b9c0c
miquelven <miquelven.silva@gmail.com>
2024-07-09 16:38:31 -0300

    feat: create filterdata file

---
+++ src/data/filterData.js
@@ -0,0 +1,24 @@
+const filterData = [
+  {
+    id: 1,
+    text: "Tudo",
+    filtered: "all",
+  },
+  {
+    id: 2,
+    text: "Masculino",
+    filtered: "man",
+  },
+  {
+    id: 3,
+    text: "Feminino",
+    filtered: "women",
+  },
+  {
+    id: 4,
+    text: "Sapatos",
+    filtered: "shoes",
+  },
+];
+
+export default filterData;
