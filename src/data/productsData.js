ff7eaaf425fbafe88714bfd8ba60de038410f6dd
miquelven <miquelven.silva@gmail.com>
2024-07-09 16:38:39 -0300

    feat: create productsdata file

---
+++ src/data/productsData.js
@@ -0,0 +1,127 @@
+import shoes1 from "../assets/imgs/shoes1.jpg";
+import shoes2 from "../assets/imgs/shoes2.jpg";
+import shoes3 from "../assets/imgs/shoes3.jpg";
+import shoes4 from "../assets/imgs/shoes4.jpg";
+import shoes5 from "../assets/imgs/shoes5.jpg";
+
+import tShirtWomen1 from "../assets/imgs/tShirtWomen1.webp";
+import tShirtWomen2 from "../assets/imgs/tShirtWomen2.webp";
+import tShirtWomen3 from "../assets/imgs/tShirtWomen3.webp";
+import tShirtWomen4 from "../assets/imgs/tShirtWomen4.webp";
+import tShirtWomen5 from "../assets/imgs/tShirtWomen5.png";
+
+import tShirtMan1 from "../assets/imgs/tShirtman1.webp";
+import tShirtMan2 from "../assets/imgs/tShirtman2.webp";
+import tShirtMan3 from "../assets/imgs/tShirtman3.webp";
+import tShirtMan4 from "../assets/imgs/tShirtman4.jpeg";
+import tShirtMan5 from "../assets/imgs/tShirtman5.webp";
+
+const productsData = [
+  {
+    id: 100,
+    text: "Sapato",
+    src: shoes1,
+    price: "45",
+    filtered: "shoes all",
+  },
+  {
+    id: 20,
+    text: "Camisa feminina",
+    src: tShirtWomen1,
+    price: "30",
+    filtered: "women all",
+  },
+  {
+    id: 30,
+    text: "Camisa Masculina",
+    src: tShirtMan1,
+    price: "40",
+    filtered: "man all",
+  },
+  {
+    id: 40,
+    text: "Sapato",
+    src: shoes2,
+    price: "45",
+    filtered: "shoes all",
+  },
+  {
+    id: 50,
+    text: "Camisa feminina",
+    src: tShirtWomen2,
+    price: "30",
+    filtered: "women all",
+  },
+  {
+    id: 60,
+    text: "Camisa Masculina",
+    src: tShirtMan2,
+    price: "40",
+    filtered: "man all",
+  },
+  {
+    id: 70,
+    text: "Sapato",
+    src: shoes3,
+    price: "45",
+    filtered: "shoes all",
+  },
+  {
+    id: 80,
+    text: "Camisa feminina",
+    src: tShirtWomen3,
+    price: "30",
+    filtered: "women all",
+  },
+  {
+    id: 90,
+    text: "Camisa Masculina",
+    src: tShirtMan3,
+    price: "40",
+    filtered: "man all",
+  },
+  {
+    id: 110,
+    text: "Sapato",
+    src: shoes4,
+    price: "45",
+    filtered: "shoes all",
+  },
+  {
+    id: 111,
+    text: "Camisa feminina",
+    src: tShirtWomen4,
+    price: "30",
+    filtered: "women all",
+  },
+  {
+    id: 120,
+    text: "Camisa Masculina",
+    src: tShirtMan4,
+    price: "40",
+    filtered: "man all",
+  },
+  {
+    id: 130,
+    text: "Sapato",
+    src: shoes5,
+    price: "45",
+    filtered: "shoes all",
+  },
+  {
+    id: 140,
+    text: "Camisa feminina",
+    src: tShirtWomen5,
+    price: "30",
+    filtered: "women all",
+  },
+  {
+    id: 150,
+    text: "Camisa Masculina",
+    src: tShirtMan5,
+    price: "40",
+    filtered: "man all",
+  },
+];
+
+export default productsData;
