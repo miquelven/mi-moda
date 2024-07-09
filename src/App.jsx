1c702b49b2570021f7109fe1aacc73ecaf87e84f
miquelven <miquelven.silva@gmail.com>
2024-07-09 10:28:22 -0300

    feat: add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Header from "./Components/ui/Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
