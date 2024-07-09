01ed15275c94261ea1d7d28134a1977cab9b082f
miquelven <miquelven.silva@gmail.com>
2024-07-09 14:15:23 -0300

    feat: create saledata file

---
+++ src/data/saleData.js
@@ -0,0 +1,29 @@
+import tShirt from "../assets/imgs/tShirt.webp";
+import outerWear from "../assets/imgs/outerwear.jpg";
+import jacket from "../assets/imgs/jacket.png";
+
+const saleData = [
+  {
+    id: 1,
+    text: "Agasalhos Masculinos",
+    sale: "40% off",
+    src: outerWear,
+    alt: "Homem vestido com agasalho",
+  },
+  {
+    id: 2,
+    text: "Camisetas Femininas",
+    sale: "25% off",
+    src: tShirt,
+    alt: "Mulher com uma camiseta",
+  },
+  {
+    id: 3,
+    text: "Jaquetas",
+    sale: "20% off",
+    src: jacket,
+    alt: "Homem vestido com uma jaqueta",
+  },
+];
+
+export default saleData;
