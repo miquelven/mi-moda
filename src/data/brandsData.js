6081ea685a7c21117d24acd99261105ecfaa890c
miquelven <miquelven.silva@gmail.com>
2024-07-09 15:20:19 -0300

    feat: brandsdata file

---
+++ src/data/brandsData.js
@@ -0,0 +1,50 @@
+import brand2 from "../assets/imgs/brand2.png";
+import brand3 from "../assets/imgs/brand3.png";
+import brand4 from "../assets/imgs/brand4.png";
+import brand5 from "../assets/imgs/brand5.png";
+import brand6 from "../assets/imgs/brand6.png";
+import brand7 from "../assets/imgs/brand7.png";
+import brand8 from "../assets/imgs/brand8.png";
+import brand9 from "../assets/imgs/brand9.png";
+import brand10 from "../assets/imgs/brand10.png";
+
+const brandsData = [
+  {
+    id: 2,
+    src: brand2,
+  },
+  {
+    id: 3,
+    src: brand3,
+  },
+  {
+    id: 4,
+    src: brand4,
+  },
+  {
+    id: 5,
+    src: brand5,
+  },
+  {
+    id: 6,
+    src: brand6,
+  },
+  {
+    id: 7,
+    src: brand7,
+  },
+  {
+    id: 8,
+    src: brand8,
+  },
+  {
+    id: 0,
+    src: brand9,
+  },
+  {
+    id: 10,
+    src: brand10,
+  },
+];
+
+export default brandsData;
