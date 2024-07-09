eae98e0d1bf91e007bb878fea0b1b204b5ed7420
miquelven <miquelven.silva@gmail.com>
2024-07-09 14:13:54 -0300

    feat: add collections and salearea components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Header from "./Components/ui/Header";
 import Banner from "./Components/Banner";
+import Collections from "./Components/Collections";
+import SaleArea from "./Components/SaleArea";
 
 function App() {
   return (
     <>
       <Header />
       <Banner />
+      <Collections />
+      <SaleArea />
     </>
   );
 }
