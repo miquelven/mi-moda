647fe85880f3ba8c8aae1a2ec78231e44ea06a84
miquelven <miquelven.silva@gmail.com>
2024-07-10 08:04:06 -0300

    style: change margin values and add 'after'

--- src/Components/FeaturesArea/index.jsx
+++ src/Components/FeaturesArea/index.jsx
@@ -4,13 +4,13 @@ import Container from "../Container";
 export default function FeaturesArea() {
   return (
     <Container>
-      <div className="mt-32  mb-80 grid grid-cols-3 justify-center items-center px-28 gap-7 max-lg:px-0 max-md:grid-cols-1">
+      <div className="mt-80  mb-80 grid grid-cols-3 justify-center items-center px-28 gap-7 max-lg:px-0 max-md:grid-cols-1">
         {featuresData.map((features) => (
           <div
             key={features.id}
             data-aos="fade-up"
             data-aos-delay={400 * features.id}
-            className="h-full w-full flex gap-5 p-4 shadow-md shadow-black/30 rounded-md max-md:w-11/12 max-md:mx-auto"
+            className="relative  h-full w-full flex gap-5 overflow-hidden p-4 shadow-md shadow-black/30 rounded-md max-md:w-11/12 max-md:mx-auto after:content-[''] after:absolute after:top-0 after:left-0 after:w-2 after:h-full after:bg-black"
           >
             <div className="">{features.icon}</div>
 
